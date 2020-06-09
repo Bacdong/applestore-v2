14acdc4c8671ffe3f457580295f51fc147d7e16a
Duong Bac Dong <duongdong2203@gmail.com>
2020-06-09 21:04:20 +0700

    latest commit

--- cart.php
+++ cart.php
@@ -34,103 +34,104 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <?php require_once('public/head.php'); ?>
-    <link rel="stylesheet" href="assets/css/cart.css">
-    <title>Apple Store | Your Cart</title>
+    <head>
+        <?php require_once('public/head.php'); ?>
+        <link rel="stylesheet" href="assets/css/cart.css">
+        <title>Apple Store | Your Cart</title>
 
-    <style>
-        section.main-cart.grid {
-            padding-bottom: 100px;
-        }
+        <style>
+            section.main-cart.grid {
+                padding-bottom: 100px;
+            }
 
-        tr,td {
-            border-bottom: 1px solid #f5f7fa;
-        }
+            tr,td {
+                border-bottom: 1px solid #f5f7fa;
+            }
 
-        tbody:last-child {
-            border-bottom: 1px solid #fff;
-        }
-    </style>
-</head>
-<body>
-    <!-- Header -->
-    <?php require_once('public/header.php'); ?>
-    <!-- /Header -->
-
-    <section class="main-cart grid">
-        <section class="main-cart__container grid wide">
-            <section class="main-cart__container-title">
-                <h4 class="title">Your Cart</h4>
-            </section>
-            
-            <section class="main-cart__container-body">
-                <section class="main-cart__left l-8">
-                    <section class="main-cart__table">
-                        <table class="table-list">
-                            <thead class="table-list_heading">
-                                <th class="heading-title">Image</th>
-                                <th class="heading-title">Name</th>
-                                <th class="heading-title">Quantity</th>
-                                <th class="heading-title">Price</th>
-                            </thead>
+            tbody:last-child {
+                border-bottom: 1px solid #fff;
+            }
+        </style>
+    </head>
     
-                            <tbody class="table-list__body">
-                                <?php 
-                                    $subtotal = 0;
-                                    foreach ($items as $item) {
-                                ?>
-                                <!-- row -->
-                                <tr class="table-list__body-row">
-                                    <td class="table-list__body-column">
-                                        <img src="<?=$item['image'];?>" alt="">
-                                    </td>
-                                    <td class="table-list__body-column"><?=$item['name'];?></td>
-                                    <td class="table-list__body-column">
-                                        <button class="btn-quantity" id="btn-quantity-down">-</button>
-                                        <input type="number" min="0" class="input-quantity" value="<?=$_SESSION['cart'][$item['id']];?>" name="">
-                                        <button class="btn-quantity" id="btn-quantity-up">+</button>
-                                    </td>
-                                    <td class="table-list__body-column">$<?php echo number_format($item['price'] * $_SESSION['cart'][$id]);?></td>
-                                    <?php $subtotal += $item['price'] * $_SESSION['cart'][$id];?>
-                                </tr>
-                                <!-- /row -->
-                                <?php } ?>
-                            </tbody>
-                        </table>
-                    </section>
+    <body>
+        <!-- Header -->
+        <?php require_once('public/header.php'); ?>
+        <!-- /Header -->
+
+        <section class="main-cart grid">
+            <section class="main-cart__container grid wide">
+                <section class="main-cart__container-title">
+                    <h4 class="title">Your Cart</h4>
                 </section>
                 
-                <section class="main-cart__right c-12 l-3 m-3">
-                    <div class="main-cart__right-top">
-                        <h4 class="main-cart__right-title">Cart Total</h4>
-    
-                        <ul class="main-cart__right-list">
-                            <li class="main-cart__right-item">
-                                Subtotal: 
-                                <span class="main-cart__right-item-span">$<?php echo number_format($subtotal);?></span>
-                            </li>
-                            <li class="main-cart__right-item">
-                                Delivery (5%): 
-                                <span class="main-cart__right-item-span">$<?php echo number_format($subtotal * 0.05)?></span>
-                            </li>
-                            <li class="main-cart__right-item">
-                                Total: 
-                                <span class="main-cart__right-item-span">$<?php echo number_format($subtotal + ($subtotal * 0.05));?></span>
-                            </li>
-                        </ul>
-                    </div>
+                <section class="main-cart__container-body">
+                    <section class="main-cart__left l-8">
+                        <section class="main-cart__table">
+                            <table class="table-list">
+                                <thead class="table-list_heading">
+                                    <th class="heading-title">Image</th>
+                                    <th class="heading-title">Name</th>
+                                    <th class="heading-title">Quantity</th>
+                                    <th class="heading-title">Price</th>
+                                </thead>
+        
+                                <tbody class="table-list__body">
+                                    <?php 
+                                        $subtotal = 0;
+                                        foreach ($items as $item) {
+                                    ?>
+                                    <!-- row -->
+                                    <tr class="table-list__body-row">
+                                        <td class="table-list__body-column">
+                                            <img src="<?=$item['image'];?>" alt="">
+                                        </td>
+                                        <td class="table-list__body-column"><?=$item['name'];?></td>
+                                        <td class="table-list__body-column">
+                                            <button class="btn-quantity" id="btn-quantity-down">-</button>
+                                            <input type="number" min="0" class="input-quantity" value="<?=$_SESSION['cart'][$item['id']];?>" name="">
+                                            <button class="btn-quantity" id="btn-quantity-up">+</button>
+                                        </td>
+                                        <td class="table-list__body-column">$<?php echo number_format($item['price'] * $_SESSION['cart'][$id]);?></td>
+                                        <?php $subtotal += $item['price'] * $_SESSION['cart'][$id];?>
+                                    </tr>
+                                    <!-- /row -->
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </section>
+                    </section>
+                    
+                    <section class="main-cart__right c-12 l-3 m-3">
+                        <div class="main-cart__right-top">
+                            <h4 class="main-cart__right-title">Cart Total</h4>
+        
+                            <ul class="main-cart__right-list">
+                                <li class="main-cart__right-item">
+                                    Subtotal: 
+                                    <span class="main-cart__right-item-span">$<?php echo number_format($subtotal);?></span>
+                                </li>
+                                <li class="main-cart__right-item">
+                                    Delivery (5%): 
+                                    <span class="main-cart__right-item-span">$<?php echo number_format($subtotal * 0.05)?></span>
+                                </li>
+                                <li class="main-cart__right-item">
+                                    Total: 
+                                    <span class="main-cart__right-item-span">$<?php echo number_format($subtotal + ($subtotal * 0.05));?></span>
+                                </li>
+                            </ul>
+                        </div>
 
-                    <div class="main-cart__right-active">
-                        <button class="btn-checkout">CHECKOUT</button>
-                    </div>
+                        <div class="main-cart__right-active">
+                            <button class="btn-checkout">CHECKOUT</button>
+                        </div>
+                    </section>
                 </section>
             </section>
         </section>
-    </section>
 
-    <!-- Footer -->
-    <?php require_once('public/footer.php'); ?>
-    <!-- /Footer -->
-</body>
+        <!-- Footer -->
+        <?php require_once('public/footer.php'); ?>
+        <!-- /Footer -->
+    </body>
 </html>
--- index.php
+++ index.php
@@ -113,159 +113,160 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>Apple Store | Home</title>
-    <?php require_once('public/head.php'); ?>
+    <head>
+        <title>Apple Store | Home</title>
+        <?php require_once('public/head.php'); ?>
 
-    <style>
-        div.alert.succes {
-            position: fixed;
-            position: relative;
-            z-index: 11;
-            top: 10px;
-            left: 10px;
-            right: 10px;
-            bottom: 10px;
-            height: 80px;
-            background-color: #27805A;
-            width: 100%;
-        }
+        <style>
+            div.alert.succes {
+                position: fixed;
+                position: relative;
+                z-index: 11;
+                top: 10px;
+                left: 10px;
+                right: 10px;
+                bottom: 10px;
+                height: 80px;
+                background-color: #27805A;
+                width: 100%;
+            }
 
-        a.pagination-item__link.active {
-            border: 1px solid #7bb6f7;
-        }
+            a.pagination-item__link.active {
+                border: 1px solid #7bb6f7;
+            }
 
-        section.go-to-top {
-            display: none;
-        }
-    </style>
-</head>
-<body>
-    <?php if(isset($_COOKIE['msg'])) { ?>
-    <!-- alert -->
-    <div class="alert success" style="display: flex; justify-content: center; position: fixed; position: absolute; z-index: 11; top: 0; left: 0; right: 0; height: 80px; background-color: transparent; width: 100%;">
-        <div class="notification" style="width: 420px; max-width: 100%; height: 100%; background-color: rgb(168, 228, 182); box-shadow: 0 1px 10px rgba(0, 0, 0, 0.3); border-radius: 8px; font-size: 17px; letter-spacing: 0.8px; display: flex; justify-content: center; align-items: center">
-            <strong>Notification! </strong> &nbsp; <?php echo $_COOKIE['msg'];?>
+            section.go-to-top {
+                display: none;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <?php if(isset($_COOKIE['msg'])) { ?>
+        <!-- alert -->
+        <div class="alert success" style="display: flex; justify-content: center; position: fixed; position: absolute; z-index: 11; top: 0; left: 0; right: 0; height: 80px; background-color: transparent; width: 100%;">
+            <div class="notification" style="width: 420px; max-width: 100%; height: 100%; background-color: rgb(168, 228, 182); box-shadow: 0 1px 10px rgba(0, 0, 0, 0.3); border-radius: 8px; font-size: 17px; letter-spacing: 0.8px; display: flex; justify-content: center; align-items: center">
+                <strong>Notification! </strong> &nbsp; <?php echo $_COOKIE['msg'];?>
+            </div>
         </div>
-    </div>
-    <!-- /alert -->
-    <?php } ?>
+        <!-- /alert -->
+        <?php } ?>
 
-    <!-- header -->
-    <?php require_once('public/header.php'); ?>
-    <!-- /header -->
+        <!-- header -->
+        <?php require_once('public/header.php'); ?>
+        <!-- /header -->
 
-    <!-- main search -->
-    <section class="main-search grid wide c-12 l-12 m-12" style="background-color: var(--white-color); position: fixed !important; position: sticky; z-index: 5; left: 0; right: 0; margin-bottom: 6rem">
-        <form action="" method="GET"class="main-search__form c-12 l-12 m-12">
-            <input name="input-search" value="" type="text" placeholder="Enter your search..." id="form-input__search" class="form-input l-10 m-10 c-8">
-            <button class="form-button l-2 m-2 c-3" id="form-button__search">Search</button>
-        </form>
-    </section>
-    <!-- /main search -->
+        <!-- main search -->
+        <section class="main-search grid wide c-12 l-12 m-12" style="background-color: var(--white-color); position: fixed !important; position: sticky; z-index: 5; left: 0; right: 0; margin-bottom: 6rem">
+            <form action="" method="GET"class="main-search__form c-12 l-12 m-12">
+                <input name="input-search" value="" type="text" placeholder="Enter your search..." id="form-input__search" class="form-input l-10 m-10 c-8">
+                <button class="form-button l-2 m-2 c-3" id="form-button__search">Search</button>
+            </form>
+        </section>
+        <!-- /main search -->
 
-    <!-- main container products -->
-    <section id="gototop" class="main">
-        <!-- latest products -->
-        <section class="main-container grid wide l-12" style="margin-top: 8rem">
-            <h4 class="latest-product__title">Latest Products</h4>
-            <section class="main-container__latest-product grid wide row c-12">
-                <!-- row -->
-                <section class="container wide row c-12" style="flex-wrap: nowrap; overflow-x: scroll;">
-                    
-                    <?php
-                        $i = 1;
-                        foreach($latest_products as $late_product) {
-                    ?>
-                    <!-- column -->
-                    <a href="product-details.php?id=<?=$late_product['id'];?>" class="container-column col c-12 l-3 m-4">
-                        <div class="products-sale">
-                            <h4 class="title-sale">New Product</h4>
-                        </div>
-                        <div class="container-product cate-<?=$i;?>">
-                            <img src="<?=$late_product['image'];?>" alt="">
-                            <div class="container-products__banner">
-                                <h2 class="products-banner__title"><?=$late_product['title'];?></h2>
-                                <p class="banner-title__description"><?=$late_product['description'];?></p>
+        <!-- main container products -->
+        <section id="gototop" class="main">
+            <!-- latest products -->
+            <section class="main-container grid wide l-12" style="margin-top: 8rem">
+                <h4 class="latest-product__title">Latest Products</h4>
+                <section class="main-container__latest-product grid wide row c-12">
+                    <!-- row -->
+                    <section class="container wide row c-12" style="flex-wrap: nowrap; overflow-x: scroll;">
+                        
+                        <?php
+                            $i = 1;
+                            foreach($latest_products as $late_product) {
+                        ?>
+                        <!-- column -->
+                        <a href="product-details.php?id=<?=$late_product['id'];?>" class="container-column col c-12 l-3 m-4">
+                            <div class="products-sale">
+                                <h4 class="title-sale">New Product</h4>
                             </div>
-                        </div>
-                        <div class="btn-show-detail">
-                            <button class="btn-detail-product">Detail</button>
-                        </div>
-                        <div class="products-description">
-                            <h3 class="products-name"><?=$late_product['name'];?></h3>
-                            <span class="products-price"><?php echo '$'.number_format($late_product['price']);?></span>
-                        </div>
-                    </a>
-                    <!-- /column -->
-                    <?php $i++; } ?>
-                   
-                </section>
-                <!-- /row -->
-            </section>
-        </section>
-        <!-- /latest products -->
-
-        <!-- products-->
-        <section class="main-container main-container__products grid wide">
-            <section class="main-container__title-trademark grid wide c-12 l-12 m-12">
-                <section class="title-products background-cate-1 c-2 l-2 m-2">
-                    <h4 id="iphone" class="latest-product__title title-name" style="font-family: var(--font-default);">Shop List</h4>
-                </section>
-            </section>
-            
-            <section class="main-container__latest-product grid wide">
-                <!-- row -->
-                <section class="container wide row c-12">
+                            <div class="container-product cate-<?=$i;?>">
+                                <img src="<?=$late_product['image'];?>" alt="">
+                                <div class="container-products__banner">
+                                    <h2 class="products-banner__title"><?=$late_product['title'];?></h2>
+                                    <p class="banner-title__description"><?=$late_product['description'];?></p>
+                                </div>
+                            </div>
+                            <div class="btn-show-detail">
+                                <button class="btn-detail-product">Detail</button>
+                            </div>
+                            <div class="products-description">
+                                <h3 class="products-name"><?=$late_product['name'];?></h3>
+                                <span class="products-price"><?php echo '$'.number_format($late_product['price']);?></span>
+                            </div>
+                        </a>
+                        <!-- /column -->
+                        <?php $i++; } ?>
                     
-                    <?php
-                        $i = 1;
-                        foreach ($productsAll as $product) {
-                    ?>
-                    <!-- column -->
-                    <a href="product-details.php?id=<?=$product['id'];?>" class="container-column col c-12 l-3 m-4">
-                        <div class="container-product cate-<?=$i?>">
-                            <img src="<?=$product['image'];?>" alt="">
-                            <!-- <div class="container-products__banner">
-                                <h2 class="products-banner__title">iPhone</h2>
-                                <p class="banner-title__description">Own, experience great moments.</p>
-                            </div> -->
-                        </div>
-                        <div class="btn-show-detail">
-                            <button class="btn-detail-product">Detail</button>
-                        </div>
-                        <div class="products-description">
-                            <h3 class="products-name"><?=$product['name'];?></h3>
-                            <span class="products-price"><?php echo '$'.$product['price'];?></span>
-                        </div>
-                    </a>
-                    <!-- /column -->
-                    <?php $i++; } ?>
-                   
+                    </section>
+                    <!-- /row -->
                 </section>
-                <!-- /row -->
-
-                <!-- pagination -->
-                <?php require_once('public/pagination.php');?>
-                <!-- /pagination -->
             </section>
+            <!-- /latest products -->
+
+            <!-- products-->
+            <section class="main-container main-container__products grid wide">
+                <section class="main-container__title-trademark grid wide c-12 l-12 m-12">
+                    <section class="title-products background-cate-1 c-2 l-2 m-2">
+                        <h4 id="iphone" class="latest-product__title title-name" style="font-family: var(--font-default);">Shop List</h4>
+                    </section>
+                </section>
+                
+                <section class="main-container__latest-product grid wide">
+                    <!-- row -->
+                    <section class="container wide row c-12">
+                        
+                        <?php
+                            $i = 1;
+                            foreach ($productsAll as $product) {
+                        ?>
+                        <!-- column -->
+                        <a href="product-details.php?id=<?=$product['id'];?>" class="container-column col c-12 l-3 m-4">
+                            <div class="container-product cate-<?=$i?>">
+                                <img src="<?=$product['image'];?>" alt="">
+                                <!-- <div class="container-products__banner">
+                                    <h2 class="products-banner__title">iPhone</h2>
+                                    <p class="banner-title__description">Own, experience great moments.</p>
+                                </div> -->
+                            </div>
+                            <div class="btn-show-detail">
+                                <button class="btn-detail-product">Detail</button>
+                            </div>
+                            <div class="products-description">
+                                <h3 class="products-name"><?=$product['name'];?></h3>
+                                <span class="products-price"><?php echo '$'.$product['price'];?></span>
+                            </div>
+                        </a>
+                        <!-- /column -->
+                        <?php $i++; } ?>
+                    
+                    </section>
+                    <!-- /row -->
+
+                    <!-- pagination -->
+                    <?php require_once('public/pagination.php');?>
+                    <!-- /pagination -->
+                </section>
+            </section>
+            <!-- /products-->
+
         </section>
-        <!-- /products-->
+        <!-- /main container products -->
 
-    </section>
-    <!-- /main container products -->
+        <!-- footer -->
+        <?php require_once('public/footer.php'); ?>
+        <!-- /footer -->
 
-    <!-- footer -->
-    <?php require_once('public/footer.php'); ?>
-    <!-- /footer -->
+        <!-- go to top -->
+        <?php require_once('public/gototop.php'); ?>
+        <!-- /go to top -->
 
-     <!-- go to top -->
-     <?php require_once('public/gototop.php'); ?>
-    <!-- /go to top -->
-
-    <!-- main javascript -->
-    <script src="assets/js/javascript.js"></script>
-    <script src="assets/js/gototop.js"></script>
-    <!-- /main javascript -->
-</body>
+        <!-- main javascript -->
+        <script src="assets/js/javascript.js"></script>
+        <script src="assets/js/gototop.js"></script>
+        <!-- /main javascript -->
+    </body>
 </html>
